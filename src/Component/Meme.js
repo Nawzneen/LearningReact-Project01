0bf53ce6697b88e1152524db8ace62571e3eea86
Nazanin Nak <earworm90@gmail.com>
2022-09-24 15:43:01 +0300

    Addint the random images upon clicking

--- src/Component/Meme.js
+++ src/Component/Meme.js
@@ -1,12 +1,25 @@
 import React from "react";
+import memesData from "../memesData.js";
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+  function memeGenerator() {
+    const memeArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const randomMemeUrl = memeArray[randomNumber].url;
+    setMemeImage(randomMemeUrl);
+  }
+  // State
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input" />
         <input type="text" className="form--input" />
-        <button className="form--button">Get a new meme image</button>
-      </form>
+        <button className="form--button" onClick={memeGenerator}>
+          Get a new meme image
+        </button>
+      </div>
+      <img className="memeUrl" src={memeImage} width="100%" />
     </main>
   );
 }
