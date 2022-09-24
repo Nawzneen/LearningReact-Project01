c605754053a4ecfda39498dbf441be96bfa26610
Nazanin Nak <earworm90@gmail.com>
2022-09-24 13:40:31 +0300

    init

---
+++ src/Component/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./images/troll-face.svg" className="header--image" />
+      <h2 className="header--title">Header Component</h2>
+      <h2 className="header--project">React Course - Project01</h2>
+    </header>
+  );
+}
